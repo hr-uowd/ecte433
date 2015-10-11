ca53e92aa9029bbb92d5942e07a9714936541c4d
unknown <hr69@hw.ac.uk>
2015-10-11 22:41:35 +0400

    added adxl345 library

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,10 @@
 #include "mbed.h"
 
-DigitalOut myled(PI_1);
-Ticker ticker1;
-Ticker ticker2;
-
-void function_1(){
-	printf("hello world\n\r");
-}
-
-void function_2(){
-	myled = !myled;
-}
 
 int main( ){
-
-	ticker1.attach(&function_1,2);
-	ticker2.attach(&function_2,0.5);
 
     while(1){
 
     }
 }
-
 
